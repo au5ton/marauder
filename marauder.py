ab5f5bbe1983fd9112287529330aadbf2fc0cfb8
Austin Jackson <themacphage@gmail.com>
2019-09-21 17:33:29 -0500

    might be finished

--- marauder.py
+++ marauder.py
@@ -2,15 +2,23 @@
 
 import os
 import argparse
+import subprocess
 
 from tqdm import tqdm
 from halo import Halo
+import humanfriendly
+from colorama import init, Fore, Back, Style
+init()
 
 parser = argparse.ArgumentParser(description='Marauder')
 parser.add_argument('source', metavar='Seeding/Movies', type=str,
                     help='Source folder')
 parser.add_argument('-o', dest='destination', metavar='local-sorted/Movies', required=True, type=str,
                     help='Destination folder')
+parser.add_argument('-c', dest='command', metavar='sleep 5', required=True, type=str,
+                help='Shell command to be run after each copy')
+parser.add_argument('-X', dest='delete_source', action='store_true',
+                help='When enabled, will delete source files')
 args = parser.parse_args()
 
 ## Calculate file size
@@ -39,9 +47,6 @@ for root, dirs, files in os.walk(os.path.abspath(args.source)):
     os.makedirs(os.path.normpath(os.path.join(os.path.abspath(args.destination), os.path.relpath(root, start=os.path.abspath(args.source)))), exist_ok=True)
 spinner.succeed()
 
-
-
-
 ## Re-create folder structure in destination
 spinner = Halo(text='Queuing files', spinner='dots')
 spinner.start()
@@ -55,19 +60,38 @@ for root, dirs, files in os.walk(os.path.abspath(args.source)):
 spinner.succeed()
 QUEUED_FILES.sort()
 
-TRAVERSED = 0
+print(f'Copying {FILE_COUNT} files')
+
+i = 1
 with tqdm(total=DISK_USAGE, unit="bytes", unit_scale=True) as t:
-    for root, dirs, files in os.walk(os.path.abspath(args.source)):
-        for name in files:
-            t.write(f'{os.path.join(root, name)} => {os.path.join(os.path.abspath(args.destination), os.path.relpath(os.path.join(root, name), start=os.path.abspath(args.source)))}')
-            with open(os.path.join(root, name), 'rb', buffering=64) as infile:
-                with open(os.path.join(os.path.abspath(args.destination), os.path.relpath(os.path.join(root, name), start=os.path.abspath(args.source))), 'wb', buffering=64) as outfile:
-                    FILE_SIZE = os.stat(os.path.join(root, name)).st_size
-                    LAST = 0
-                    # copy file
-                    while infile.tell() < FILE_SIZE:
-                        outfile.write(infile.read())
-                        t.update(infile.tell() - LAST)
-                        LAST = infile.tell()
-                    # run command (upload)
-                    # delete source file
+    for item in QUEUED_FILES:
+        # unwrap tuple
+        FILE_SIZE, SRC, DESTINATION = item
+        t.write(f'[{i} / {FILE_COUNT}] {Fore.YELLOW}{os.path.relpath(SRC, start=os.path.abspath(args.source))} @ {humanfriendly.format_size(FILE_SIZE, binary=True)}{Fore.RESET}')
+        with open(SRC, 'rb', buffering=64) as infile:
+            with open(DESTINATION, 'wb', buffering=64) as outfile:
+                # copy file
+                LAST = 0
+                t.write(f'\t{Fore.YELLOW}Started copy... {Fore.RESET}')
+                while infile.tell() < FILE_SIZE:
+                    outfile.write(infile.read())
+                    t.update(infile.tell() - LAST)
+                    LAST = infile.tell()
+                t.write(f'\t{Fore.YELLOW}Finished copy {Fore.RESET}{Fore.GREEN}✔{Fore.RESET}')
+                # run command (upload)
+                t.write(f'\t{Fore.YELLOW}Waiting for command to exit: {args.command} {Fore.RESET}')
+                subprocess.run(args.command.split(' '))
+                t.write(f'\t{Fore.YELLOW}Exited! {Fore.GREEN}✔{Fore.RESET}')
+        # delete source file
+        if args.delete_source:
+            t.write(f'\t{Fore.YELLOW}Deleting source and desination... {Fore.RESET}', end='')
+            os.remove(SRC)
+            os.remove(DESTINATION)
+            t.write(f'\t{Fore.YELLOW}Deleting source and desination... {Fore.RESET}{Fore.GREEN}✔{Fore.RESET}')
+        else:
+            t.write(f'\t{Fore.YELLOW}Deleting desination... {Fore.RESET}', end='')
+            os.remove(DESTINATION)
+            t.write(f'\t{Fore.YELLOW}Deleting desination... {Fore.RESET}{Fore.GREEN}✔{Fore.RESET}')
+        i += 1
+            
+        
